8e32a50adaa169130853f304b5b8ba1539b530ea
dohee <rhehgml00@naver.com>
2024-08-06 23:51:02 +0900

    오픈그래프

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { Helmet } from "react-helmet";
 import "./App.css";
 //컴포넌트 import
 import Landing from "./pages/Landing";
